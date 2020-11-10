f902df49a34c9fbc3609fd3e4693233ef8b65052
Who1sthatdude <62994956+Who1sthatdude@users.noreply.github.com>
2020-11-10 15:47:52 +0200

    message

--- src/com/jabalab/Main.java
+++ src/com/jabalab/Main.java
@@ -15,6 +15,15 @@ public class Main {
             System.out.print(item + " ");
         }
         System.out.println();
+
+        Nutrition.Meat obj1 = new Nutrition.Meat("fired", 100);
+        Nutrition.Meat obj2 = new Nutrition.Meat("fired", 100);
+        if(obj1.equals(obj2)){
+            System.out.println("Equals");
+        }else {
+            System.out.println("Not equals");
+        }
+
         Axe object1 = new Axe(Size.Large, 100);
         Axe object2 = new Axe(Size.Large, 100);
         if(object1.equals(object2)){
