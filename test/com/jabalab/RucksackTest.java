8b3460563fe0388bb8350666e3a2047d79fee96d
Who1sthatdude <62994956+Who1sthatdude@users.noreply.github.com>
2020-12-01 13:13:24 +0200

    added integration test.

--- test/com/jabalab/RucksackTest.java
+++ test/com/jabalab/RucksackTest.java
@@ -8,7 +8,34 @@ import org.mockito.Mockito;
 
 
 public class RucksackTest {
+    @Test
+    public void testGetTool(){
+        Tool toolMock = Mockito.mock(Axe.class);
+        String str = "Axe";
+        Mockito.when(toolMock.getType()).thenReturn(str);
+        Rucksack obj = new Rucksack();
+        try {
+            obj.takeTool(toolMock, str);
+        }catch (MyException e){
+            e.printStackTrace();
+        }
+        Assertions.assertEquals(obj.getTool(str).getType(), str);
+        Mockito.verify(toolMock).getType();
+    }
 
+    @Test
+    public void testGetFood(){
+        Nutrition.Meat meatMock = Mockito.mock(Nutrition.Meat.class);
+        Mockito.when(meatMock.getWeight()).thenReturn(1.0);
+        Rucksack obj = new Rucksack();
+        try{
+            obj.takeMeat(meatMock);
+        }catch (MyException e){
+            e.printStackTrace();
+        }
+        Assertions.assertEquals(obj.getFood().getMeat(0).getWeight(), 1.0);
+        Mockito.verify(meatMock).getWeight();
+    }
 
 }
 
